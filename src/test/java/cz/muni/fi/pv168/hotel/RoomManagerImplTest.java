0a35a3adfdebfcee0c488882e89a2ee46a270d62
Tomáš Livora <livoratom@gmail.com>
2013-03-19 08:54:35 +0100

    Created tests for RoomManager, reversed tests for GuestManager + created RoomType

---
+++ src/test/java/cz/muni/fi/pv168/hotel/RoomManagerImplTest.java
@@ -0,0 +1,90 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cz.muni.fi.pv168.hotel;
+
+import java.util.Collection;
+import static junit.framework.Assert.assertEquals;
+import static junit.framework.Assert.assertNotNull;
+import junit.framework.TestCase;
+
+/**
+ *
+ * @author livthomas
+ */
+public class RoomManagerImplTest extends TestCase {
+	
+	public RoomManagerImplTest(String testName) {
+		super(testName);
+	}
+	
+	@Override
+	protected void setUp() throws Exception {
+		super.setUp();
+	}
+	
+	@Override
+	protected void tearDown() throws Exception {
+		super.tearDown();
+	}
+
+	/**
+	 * Test of createRoom method, of class RoomManagerImpl.
+	 */
+	public void testCreateRoom() {
+		System.out.println("createRoom");
+		RoomManager manager = new RoomManagerImpl();
+		Room room = newRoom(RoomType.APPARTMENTS, (short)4, true, null);
+		
+		// ID assignation
+		Room result = manager.createRoom(room);
+		assertNotNull(result.getId());
+		
+		// room insertion
+		room.setId(result.getId());
+		assertEquals(room, result);
+	}
+
+	/**
+	 * Test of updateRoom method, of class RoomManagerImpl.
+	 */
+	public void testUpdateRoom() {
+		System.out.println("updateRoom");
+		RoomManager manager = new RoomManagerImpl();
+		
+		Room room = manager.createRoom(newRoom(RoomType.APPARTMENTS, (short)4, true, null));
+		room.setNote("Very dirty");
+		
+		Room result = manager.updateRoom(room);
+		assertEquals(room, result);
+	}
+
+	/**
+	 * Test of deleteRoom method, of class RoomManagerImpl.
+	 */
+	public void testDeleteRoom() {
+		System.out.println("deleteRoom");
+		RoomManager manager = new RoomManagerImpl();
+		Room room1 = newRoom(RoomType.APPARTMENTS, (short)4, true, null);
+		Room room2 = newRoom(RoomType.APPARTMENTS, (short)6, false, null);
+		
+		manager.createRoom(room1);
+		manager.createRoom(room2);
+		
+		manager.deleteRoom(room1);
+		
+		assertNull(manager.findRoomById(room1.getId()));
+		assertNotNull(manager.findRoomById(room2.getId()));
+	}
+	
+	private Room newRoom(RoomType type, short beds, boolean seaView, String note) {
+		Room room = new Room();
+		room.setType(type);
+		room.setBeds(beds);
+		room.setSeaView(seaView);
+		room.setNote(note);
+		return room;		
+	}
+	
+}
