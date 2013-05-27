1c581c93c00660bb12579e81d0ffdd8e04a62ff8
wintermute <wintermute@dhcp-237-134.eduroam.muni.cz>
2013-05-27 12:23:12 +0200

    create properties files for i18n purpose

--- src/main/java/cz/muni/fi/pv168/hotel/gui/RoomFrame.java
+++ src/main/java/cz/muni/fi/pv168/hotel/gui/RoomFrame.java
@@ -120,7 +120,7 @@ public class RoomFrame extends javax.swing.JFrame {
                     .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addComponent(jLabelNote))
                 .addGap(18, 18, 18)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addComponent(jButtonSave, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addComponent(jButtonCancel))
                 .addContainerGap(20, Short.MAX_VALUE))
